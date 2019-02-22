f1c738ca34dcafcdfee6dc5f2e74fcc429f0e30a
Mateusz Kubaczyk <mkubaczyk@egnyte.com>
2019-02-22 23:47:58 +0100

    Make it work with external k8s

--- scripts/run.sh
+++ scripts/run.sh
@@ -14,9 +14,12 @@ function init_minikube {
     minikube delete || true
     log "Bootstraping minikube"
     minikube start --vm-driver=hyperkit --kubernetes-version=v1.13.2 --cpus=4 --memory=8192
+}
+
+function init_helm {
     log "Initializing helm"
-    kubectl create serviceaccount --namespace kube-system tiller
-    kubectl create clusterrolebinding tiller-admin --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+    kubectl create serviceaccount --namespace kube-system tiller || true
+    kubectl create clusterrolebinding tiller-admin --clusterrole=cluster-admin --serviceaccount=kube-system:tiller || true
     helm init --upgrade --service-account tiller --wait
 }
 
@@ -39,14 +42,24 @@ function run_echo {
 
 function run_canary_header {
     controller=$1
+    is_external=$2
     log "Running http header match check for $controller provider with -H \"will-you-let-me-in: always\" curl parameter..."
     command="./scripts/count.sh 100"
     host="echo.minikube"
-    if [[ $controller == "istio" ]]
-    then
-        command="${command} $(minikube ip):$(kubectl get svc istio-ingressgateway -o jsonpath="{.spec.ports[0].nodePort}")"
+    if [[ $is_external == "true" ]]; then
+        if [[ $controller == "istio" ]]
+        then
+            command="${command} $(kubectl get svc istio-ingressgateway -o jsonpath="{.status.loadBalancer.ingress[0].ip}")"
+        else
+            command="${command} $(kubectl get svc nginx-ingress-controller -o jsonpath="{.status.loadBalancer.ingress[0].ip}")"
+        fi
     else
-        command="${command} $(minikube ip):$(kubectl get svc nginx-ingress-controller -o jsonpath="{.spec.ports[0].nodePort}")"
+        if [[ $controller == "istio" ]]
+        then
+            command="${command} $(minikube ip):$(kubectl get svc istio-ingressgateway -o jsonpath="{.spec.ports[0].nodePort}")"
+        else
+            command="${command} $(minikube ip):$(kubectl get svc nginx-ingress-controller -o jsonpath="{.spec.ports[0].nodePort}")"
+        fi
     fi
     command="${command} $host 'will-you-let-me-in: always'"
     eval ${command}
@@ -54,17 +67,30 @@ function run_canary_header {
 
 function run_canary {
     controller=$1
+    is_external=$2
     log "Running weighted routing check for ${controller}..."
     command="./scripts/count.sh 100"
     host="echo.minikube"
-    if [[ $controller == "istio" ]]
-    then
-        command="${command} $(minikube ip):$(kubectl get svc istio-ingressgateway -o jsonpath="{.spec.ports[0].nodePort}")"
-    elif [[ $controller == "nginx" ]]
-    then
-        command="${command} $(minikube ip):$(kubectl get svc nginx-ingress-controller -o jsonpath="{.spec.ports[0].nodePort}")"
+    if [[ $is_external == "true" ]]; then
+        if [[ $controller == "istio" ]]
+        then
+            command="${command} $(kubectl get svc istio-ingressgateway -o jsonpath="{.status.loadBalancer.ingress[0].ip}")"
+        elif [[ $controller == "nginx" ]]
+        then
+            command="${command} $(kubectl get svc nginx-ingress-controller -o jsonpath="{.status.loadBalancer.ingress[0].ip}")"
+        else
+            command="${command} $(kubectl get svc traefik -o jsonpath="{.status.loadBalancer.ingress[0].ip}")"
+        fi
     else
-        command="${command} $(minikube ip):$(kubectl get svc traefik -o jsonpath="{.spec.ports[0].nodePort}")"
+        if [[ $controller == "istio" ]]
+        then
+            command="${command} $(minikube ip):$(kubectl get svc istio-ingressgateway -o jsonpath="{.spec.ports[0].nodePort}")"
+        elif [[ $controller == "nginx" ]]
+        then
+            command="${command} $(minikube ip):$(kubectl get svc nginx-ingress-controller -o jsonpath="{.spec.ports[0].nodePort}")"
+        else
+            command="${command} $(minikube ip):$(kubectl get svc traefik -o jsonpath="{.spec.ports[0].nodePort}")"
+        fi
     fi
     command="${command} $host"
     eval ${command}
@@ -79,6 +105,7 @@ function run_traefik {
 case "$1" in
     all)
         init_minikube
+        init_helm
         run_istio
         run_traefik
         run_nginx_ingress
@@ -91,6 +118,9 @@ case "$1" in
         ;;
     init)
         init_minikube
+        ;;
+    helm)
+        init_helm
         ;;
     istio)
         run_istio
@@ -107,20 +137,35 @@ case "$1" in
     canary_istio)
         run_canary istio
         ;;
+    canary_istio_external)
+        run_canary istio true
+        ;;
     canary_traefik)
         run_canary traefik
+        ;;
+    canary_traefik_external)
+        run_canary traefik true
         ;;
     canary_nginx)
         run_canary nginx
         ;;
+    canary_nginx_external)
+        run_canary nginx true
+        ;;
     canary_istio_header)
         run_canary_header istio
+        ;;
+    canary_istio_header_external)
+        run_canary_header istio true
         ;;
     canary_nginx_header)
         run_canary_header nginx
         ;;
+    canary_nginx_header_external)
+        run_canary_header nginx true
+        ;;
     *)
-        log "Unknown parameter (all|init|istio|traefik|echo|canary_istio|canary_traefik|header_traefik)"
+        log "Unknown parameter (all|init|helm|istio|traefik|echo|canary_istio|canary_traefik|header_traefik)"
         exit 1
         ;;
 esac
